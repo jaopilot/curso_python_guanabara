92caf73cc7a2905bdfe07a903b7efc23a255b754
jaopilot <joaopedrornascimento@outlook.com>
2025-04-17 23:40:29 -0300

    Initialize Pygame mixer and set music playback options in ex021.py

--- exercicios_mundo1/ex021.py
+++ exercicios_mundo1/ex021.py
@@ -1,7 +1,8 @@
 import pygame
+pygame.mixer.init()
 pygame.init()
-pygame.mixer.music.set_volume(1.0)  # Volume máximo (1.0)
-pygame.mixer.music.load(r'E:\Documents\Projetos Phyton\curso_python_guanabara\arquivos_de_exercicios\amongus.mp3')
-pygame.mixer.music.play()
+pygame.mixer.music.load('arquivos_de_exercicios/amongus.mp3')
+pygame.mixer.music.play(loops=1, start=0.0)
+input()
 pygame.event.wait()
 # Exercicio 21
